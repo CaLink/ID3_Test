c0e68d87151f427f9c62c97f820ffecca4f7ccdf
CaLink <caplink777@gmail.com>
2019-11-25 13:39:48 +1000

    Добавьте файлы проекта.

---
+++ ID3_Test/ID3v2.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ID3_Test
+{
+    class ID3v2
+    {
+        string way;
+        public byte[] sign = new byte[3];
+        public byte[] version = new byte[2];
+        public byte flags;
+        public byte[] byteSize = new byte[4];
+        public int size;
+
+
+        public ID3v2(string way)
+        {
+            using (FileStream fs = new FileStream(way, FileMode.Open, FileAccess.Read))
+            using (BinaryReader br = new BinaryReader(fs))
+            {
+                this.way = way;
+                GetHeader(this.way,br);
+                GetFrame_v34(br);
+            }
+        }
+
+        void GetHeader(string way,BinaryReader br)
+        {
+            
+            {
+                ByteReader(sign, br);
+                ByteReader(version, br);
+                flags = br.ReadByte();
+                ByteReader(byteSize, br);
+                size = BitConverter.ToInt32(byteSize, 3);                
+            }
+        }
+
+        void ByteReader(byte[] array,BinaryReader br)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = br.ReadByte();
+            }
+        }
+
+        void GetFrame_v2(BinaryReader br)
+        {
+            
+        }
+
+        
+
+        void GetFrame_v34(BinaryReader br)
+        {
+
+           // while (size > br.BaseStream.Position)
+            {
+                byte[] id = new byte[4];
+                int size; // Какие-то лютые траблы с Size'ом
+                byte[] flags = new byte[2];
+                byte[] content;
+
+             
+                ByteReader(id, br);
+                size = br.ReadInt32();
+                ByteReader(flags, br);
+
+                content = new byte[size];
+                ByteReader(content, br);
+
+            }
+        }
+
+        public void Writer()
+        {
+            foreach (byte item in sign)
+                Console.Write(item + " ");
+            Console.WriteLine();
+            foreach (byte item in sign)
+                Console.Write((char)item + " ");
+            Console.WriteLine();
+
+            foreach (byte item in version)
+                Console.Write(item + " ");
+            Console.WriteLine();
+            foreach (byte item in version)
+                Console.Write((char)item + " ");
+            Console.WriteLine();
+
+            Console.WriteLine(flags);
+            Console.WriteLine(flags.ToString());
+
+            Console.WriteLine(size);
+
+        }
+    }
+}
---
+++ ID3_Test/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ID3_Test
+{
+    class Program
+    {
+        static string way = @"C:\Users\SaSnyUser\Desktop\Test.mp3";
+        //static List<byte> file = new List<byte>();
+
+
+
+        static void Main(string[] args)
+        {
+            bool id3_type;
+            ID3v2 test;
+
+            using (FileStream fs = new FileStream(way, FileMode.Open, FileAccess.Read))
+            using (BinaryReader br = new BinaryReader(fs))
+                id3_type = ID3_Info(br);
+
+            if (id3_type)
+            { 
+                test = new ID3v2(way);
+                test.Writer();
+            }
+          
+        }
+
+
+        static bool ID3_Info(BinaryReader br)
+        {
+            string ans;
+            ans = ((char)br.ReadByte()).ToString() + ((char)br.ReadByte()).ToString() + ((char)br.ReadByte()).ToString();
+            if (ans == "ID3")
+                return true;
+            else
+                return false;
+        }
+    }
+}
